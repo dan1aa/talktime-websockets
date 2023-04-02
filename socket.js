79ef53e7e109499a1efdb81632b25c416a716d46
dan1aa <burmeidan@gmail.com>
2023-04-02 09:07:34 +0300

    first

---
+++ socket.js
@@ -0,0 +1,21 @@
+const { Server } = require('ws')
+
+const server = new Server({ port: 3000 })
+const WEBSOCKET_OPEN = 1
+
+function startUpSocket() {
+    server.on('connection', ws => {
+        ws.on('message', message => {
+            const convertedMessage = message.toString()
+            console.log(convertedMessage)
+            server.clients.forEach(client => {
+                if (client.readyState === WEBSOCKET_OPEN) {
+                    client.send(JSON.stringify({message: convertedMessage}))
+                }
+            })
+        })
+
+    })
+}
+
+startUpSocket()
