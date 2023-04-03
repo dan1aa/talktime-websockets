08966ab17779c86bf1916e27b5eac5e8429f6e0b
dan1aa <burmeidan@gmail.com>
2023-04-03 15:42:04 +0300

    test

--- socket.js
+++ socket.js
@@ -1,21 +1,21 @@
 const { Server } = require('ws')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
 
 const server = new Server({ port: 3000 })
 const WEBSOCKET_OPEN = 1
 
-function startUpSocket() {
-    server.on('connection', ws => {
-        ws.on('message', message => {
-            const convertedMessage = message.toString()
-            console.log(convertedMessage)
-            server.clients.forEach(client => {
-                if (client.readyState === WEBSOCKET_OPEN) {
-                    client.send(JSON.stringify({message: convertedMessage}))
-                }
-            })
+app.listen(5000, () => {
+    console.log('Server started')
+})
+
+server.on('connection', ws => {
+    ws.on('message', data => {
+        server.clients.forEach(client => {
+            client.send(JSON.stringify(data))
         })
-
-    })
-}
-
-startUpSocket()
+    });
+});
