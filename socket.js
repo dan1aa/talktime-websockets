2399458142b566b20b5cb660b1f99f8021452810
dan1aa <burmeidan@gmail.com>
2023-04-04 09:49:56 +0300

    test package

--- socket.js
+++ socket.js
@@ -4,17 +4,17 @@ const cors = require('cors')
 
 let app = express()
 
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 app.use(express.static('public'));
 
-const server = new WebSocket.Server({ server:app.listen(5000) });
+const server = new WebSocket.Server({ server: app.listen(5000) });
 
 app.use(cors())
 
 server.on('connection', (socket) => {
-    socket.on('message', (msg) => {
-      server.clients.forEach( client => {
-        client.send(JSON.stringify(msg));
-      })
-    });
+  socket.on('message', (msg) => {
+    server.clients.forEach(client => {
+      client.send(JSON.stringify(msg));
+    })
   });
+});
